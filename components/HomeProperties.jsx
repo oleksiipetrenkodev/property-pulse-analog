973d269325ea724046bc9ae655ca6f3839cd74b7
oleksiipetrenkodev <apetrenko.job@gmail.com>
2025-04-28 22:09:11 +0300

    added recent properties from db

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,9 +1,15 @@
-import properties from "@/properties.json";
 import PropertyCard from "@/components/PropertyCard";
 import Link from "next/link";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
-const HomeProperies = () => {
-  const recentProperties = properties.slice(0, 3);
+const HomeProperies = async () => {
+  await connectDB();
+
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
 
   return (
     <>
@@ -12,7 +18,7 @@ const HomeProperies = () => {
           <h2 className="text-3xl font-bold text-blue-500 mb-6">
             Recent Properties
           </h2>
-          {properties.length === 0 ? (
+          {recentProperties.length === 0 ? (
             <p>No properties found</p>
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
