c784d2be8dfc889e43e918fc03c1d8b73d5d7acc
oleksiipetrenkodev <apetrenko.job@gmail.com>
2025-05-01 20:27:21 +0300

    added PropertyDetails page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,5 @@
 import NotFoundPage from "@/app/not-found";
+import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
@@ -34,7 +35,7 @@ const PropertyPage = async ({ params }) => {
         <section className="bg-blue-50">
           <div className="container m-auto py-10 px-6">
             <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-              {/* Property Info */}
+              <PropertyDetails property={property} />
             </div>
           </div>
         </section>
